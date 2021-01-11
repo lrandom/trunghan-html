87446499090198d7881d31137606ffb8da649c03
root <luann4099@gmail.com>
2021-01-11 09:46:30 +0700

    jquery

---
+++ session-7/main.js
@@ -0,0 +1,27 @@
+/*dat code jquery va js vao day*/
+function getHTML() {
+    /*JS THUAN*/
+    /*const node = document.querySelector('.html');
+    const html = node.innerHTML;*/
+
+    const html = $('.html').html();
+    $('.html').html('<span>wewewe</span>');
+    alert(html);
+}
+
+function removeHTML() {
+    $('.remove').remove();
+}
+
+function afterMe() {
+    $('.after').after('<div>I am after you</div>')
+}
+
+function getValue() {
+    const val = $('.input').val();
+    alert(val);
+}
+
+function empty() {
+    $('.empty').empty();
+}
